122d13630c0ed8d25e5707b8fd7e825beb5fc8ea
gagovski <gagovski@163.com>
2018-04-08 11:30:52 +0800

    fix the app dead when the screen change oritation

--- app/src/main/java/gc/com/todoapp/tasklist/TasklistFragment.java
+++ app/src/main/java/gc/com/todoapp/tasklist/TasklistFragment.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -17,6 +21,9 @@ import gc.com.todoapp.db.TaskData;
 
 public class TasklistFragment extends Fragment implements TasklistContract.View {
     private TasklistContract.Presenter m_presenter;
+    private boolean m_editfinish = false;
+    private TextView m_textEdit;
+    private EditText m_titleText;
     private static final String TAG = "TasklistFragment";
 
     public static TasklistFragment newInstance() {
@@ -27,9 +34,19 @@ public class TasklistFragment extends Fragment implements TasklistContract.View 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e(TAG, "onCreate");
-        List<TaskData>list =  m_presenter.queryTasklist(((TasklistActivity)getActivity()).getId());
-        for (TaskData t : list) {
-            Log.e(TAG, t.toString());
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Log.e(TAG, "onViewCreated");
+        if (m_presenter != null) {
+            List<TaskData>list =  m_presenter.queryTasklist(((TasklistActivity)getActivity()).getId());
+            for (TaskData t : list) {
+                Log.e(TAG, t.toString());
+            }
         }
     }
 
@@ -44,6 +61,50 @@ public class TasklistFragment extends Fragment implements TasklistContract.View 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_tasklist, container, false);
+        m_textEdit = root.findViewById(R.id.tv_edit);
+        m_textEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "click edit");
+                if (m_editfinish) {
+                    String title = m_titleText.getText().toString();
+                    m_titleText.setText(null);
+                    m_titleText.clearFocus();
+                    Log.e(TAG, "title: " + title);
+                }
+            }
+        });
+        m_titleText = root.findViewById(R.id.newtask_title);
+        m_titleText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    Log.e(TAG, "hasfocus true");
+                    m_textEdit.setText("Add");
+                } else {
+                    Log.e(TAG, "hasfocus false");
+                }
+            }
+        });
+        m_titleText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.e(TAG, "beforeTextChanged");
+                m_editfinish = false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.e(TAG, "onTextChanged");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.e(TAG, "afterTextChanged");
+                m_editfinish = true;
+            }
+        });
+
         return root;
     }
 
